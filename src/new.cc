3c2e912229729f0dc136816e419990247e0c609a
PranavMenon25 <pranavmenon2019@gmail.com>
2024-06-28 08:08:09 +0530

    Initial commit

---
+++ src/new.cc
@@ -0,0 +1,22 @@
+#include<vector>
+#include<map>
+#include<iostream>
+using namespace std;
+
+
+
+class Added_class{
+    public:
+        static vector<int> set_acces;
+        static map<int,int> freq_map;
+        static vector<int> set_cur_length;
+};
+
+class tag_directory_new{
+    public:
+        static std::vector<std::vector<std::pair<uint64_t, pair<int, bool>>>> tag_direc;
+        static vector<vector<int>> srrip;
+    
+};
+
+
